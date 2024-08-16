2ef347fe04733516ca77614f764120441a4d95ae
ashritha1811 <ashrithaindha@gmail.com>
2024-08-16 02:50:10 -0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,24 @@
+
+const users = [
+    { username: 'user1', password: 'password1' },
+    { username: 'user2', password: 'password2' }
+];
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form from submitting the traditional way
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const message = document.getElementById('message');
+
+    
+    const user = users.find(user => user.username === username && user.password === password);
+
+    if (user) {
+        message.textContent = 'Login successful!';
+        message.style.color = 'green';
+    } else {
+        message.textContent = 'Invalid username or password.';
+        message.style.color = 'red';
+    }
+});
